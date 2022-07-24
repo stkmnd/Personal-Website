4f64703e42fd40926563f630f875aaa667b9c6c5
stkmnd <63492158+stkmnd@users.noreply.github.com>
2022-07-24 18:28:01 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const card = document.querySelector(".card__inner");
+
+card.addEventListener("click", function () {
+  card.classList.toggle('is-flipped');
+});
+
+const card0 = document.querySelector(".card__inner0")
+
+card0.addEventListener("click", function () {
+    card0.classList.toggle('is-flipped');
+  });
+
+  const card1 = document.querySelector(".card__inner1")
+
+  card1.addEventListener("click", function () {
+      card1.classList.toggle('is-flipped');
+    });
