2f07c82f56508d987e43b07b01c871e5329c616d
Sarthak Mundhra <63492158+stkmnd@users.noreply.github.com>
2022-08-16 22:57:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const card = document.querySelector(".card__inner");
 
 card.addEventListener("click", function () {
   card.classList.toggle('is-flipped');
-});
+  });
 
 const card0 = document.querySelector(".card__inner0")
 
@@ -10,8 +10,14 @@ card0.addEventListener("click", function () {
     card0.classList.toggle('is-flipped');
   });
 
-  const card1 = document.querySelector(".card__inner1")
+const card1 = document.querySelector(".card__inner1")
 
-  card1.addEventListener("click", function () {
-      card1.classList.toggle('is-flipped');
-    });
+card1.addEventListener("click", function () {
+    card1.classList.toggle('is-flipped');
+  });
+
+const card2 = document.querySelector(".card__inner2");
+
+card2.addEventListener("click", function () {
+  card2.classList.toggle('is-flipped');
+  });
